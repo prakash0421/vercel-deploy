b83563ff786ed3719e76126ce8c83e2dea5a8f99
prakash0421 <prakashsd2104@gmail.com>
2024-08-13 18:56:00 +0530

    Update settings.py

--- medical_project/settings.py
+++ medical_project/settings.py
@@ -75,10 +75,20 @@ LOGOUT_REDIRECT_URL='login'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres.iqnadtlrkusqlsrmhnjg',
+        'PASSWORD': 's8V4s6LqtzjQInsD',
+        'HOST': 'aws-0-ap-south-1.pooler.supabase.com',
+        'PORT': '6543',
     }
 }
 
